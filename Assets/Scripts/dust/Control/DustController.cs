798b05db20d18a682410a5da9d2cd7aca32354b5
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2018-01-06 16:59:57 +0200

    Controllers

---
+++ Assets/Scripts/dust/Control/DustController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dust
+{
+	
+	public enum Action
+	{
+		RIGHT,
+		LEFT,
+		JUMP,
+		PUSH,
+		STAY,
+	}
+
+	public abstract class DustContoller : MonoBehaviour
+	{
+		/// <summary>
+		/// Get the Action to preform at this time.
+		/// </summary>
+		/// <returns>The action.</returns>
+		public abstract Action getAction ();
+		
+	}
+}
---
+++ Assets/Scripts/dust/Control/KeyBoardController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dust
+{
+
+	public class KeyBoardController : DustContoller
+	{
+
+		public KeyCode rightKey;
+		public KeyCode leftKey;
+		public KeyCode jumpKey;
+		public KeyCode pushKey;
+	
+		public override Action getAction ()
+		{
+
+			if (Input.GetKey (jumpKey)) {
+				return Action.JUMP;
+			} else if (Input.GetKey (pushKey)) {
+				return Action.PUSH;
+			} else if (Input.GetKey (rightKey)) {
+				return Action.RIGHT;
+			} else if (Input.GetKey (leftKey)) {
+				return Action.LEFT;
+			} else {
+				return Action.STAY;
+			}
+		}
+	}
+}

806ecec95cc81c9d0000fad4c5fa1060b3fe795c
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2018-01-08 19:15:01 +0200

    backupweird

---
+++ Assets/Scripts/dust/Environment/Hammer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dust
+{
+	public class Hammer : MonoBehaviour
+	{
+
+		public enum NOTE {
+			A,
+			B,
+			C,
+			D,
+			E,
+		}
+
+		public NOTE note;
+
+
+
+		// Use this for initialization
+		void Start ()
+		{
+
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+
+		public NOTE getNote() {
+			return note;
+		}
+	}
+}
---
+++ Assets/Scripts/dust/Environment/HammersManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dust
+{
+	public class HammersManager : MonoBehaviour
+	{
+
+		public List<Hammer> hammers;
+
+		private Dictionary<Hammer.NOTE, Hammer> hammersDict_;
+
+	// Use this for initialization
+	void Start ()
+		{
+			hammersDict_ = new Dictionary<Hammer.NOTE, Hammer>();
+			foreach (Hammer hammer in hammers) {
+				hammersDict_ [hammer.getNote ()] = hammer;
+			}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+		
+		}
+	}
+}

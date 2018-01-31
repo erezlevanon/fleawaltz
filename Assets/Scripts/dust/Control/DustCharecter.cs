224cac8af1c0b964be9df269a6491183eaeb08ae
Erez Levanon <erez.leavnon@mail.huji.ac.il>
2018-01-31 12:33:22 +0200

    resume work

--- Assets/Scripts/dust/Control/DustCharecter.cs
+++ Assets/Scripts/dust/Control/DustCharecter.cs
@@ -11,30 +11,50 @@ public class DustCharecter : MonoBehaviour {
 		private Rigidbody2D body;
 		private bool isJumping;
 		private long  isPushing;
+		private int facing;
 
 		[Header("Movement")]
 		public float verticalMultiplier;
 		public float horizontalMultiplier;
+		public float turboMul;
+		public float turboCooldown;
+
+		private float lastTurbo;
+
+
+		private Animator animator;
 
 	// Use this for initialization
 	void Start () {
 			body = GetComponent<Rigidbody2D> ();
+			animator = GetComponent<Animator> ();
+			lastTurbo = Time.time;
+			facing = 1;
 	}
-	
+			
 	// Update is called once per frame
 	void FixedUpdate () {
 			Vector2 velocity = body.velocity;
+			Vector2 scale = body.transform.localScale;
 			Action action = controller.getAction ();
 			if (action == Action.JUMP && this.transform.localPosition.y < -1f) {
 				velocity.y = verticalMultiplier;
 			} else if (action == Action.RIGHT) {
+				facing = 1;
 				velocity.x = horizontalMultiplier;
 			} else if (action == Action.LEFT) {
 				velocity.x = -horizontalMultiplier;
+				facing = -1;
+			} else if (action == Action.PUSH && Time.time - lastTurbo > turboCooldown) {
+				velocity.x = facing * turboMul;
+				lastTurbo = Time.time;
+				animator.SetTrigger ("Turbo");
 			} else if (action == Action.STAY) {
 				velocity.x = 0;
 			}
 			body.velocity = velocity;
+			scale.x = Mathf.Abs(scale.x) * facing;
+			body.transform.localScale = scale;
 	}
 
 		void OnCollisionEnter2D (Collision2D col) {
